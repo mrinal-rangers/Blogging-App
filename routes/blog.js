e24ce6279390b700f45a4cb91b36d934a8f54ba7
Mrinal Deb <mrinal437a@gmail.com>
2024-02-01 18:43:58 +0530

    added comment functionality , project completed

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const router  = Router();
 const multer  = require('multer');
 const Blog= require('../models/blog')
 const path = require('path');
+const Comment = require('../models/comment')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -23,13 +24,26 @@ router.get('/add-new',(req,res)=>{
     });
 })
 
+router.post('/comment/:blogId',async(req,res)=>{
+  await Comment.create({
+    content:req.body.content,
+    createdBy:req.user._id,
+    blogId: req.params.blogId,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`)
+})
+
 router.get('/:id',async(req,res)=>{
   const blog = await Blog.findById(req.params.id).populate('createdBy');
+  const blogId = req.params.id;
+  const comments = await Comment.find({blogId:blogId}).populate('createdBy');
   return res.render('blog',{
       user:req.user,
       blog:blog,
+      comments:comments,
   });
 })
+
 router.post('/',upload.single('coverImage'),async(req,res)=>{
     const {title,body}= req.body;
     const blog = await Blog.create({
